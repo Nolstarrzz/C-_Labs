93286620d976469349b0f5f6ddea4fb3aab7e4e7
Nolan <125843678+Nolstarrzz@users.noreply.github.com>
2025-09-10 15:24:24 -0700

    Added spaces

--- Milage_Lab/milage.cpp
+++ Milage_Lab/milage.cpp
@@ -7,7 +7,7 @@ int main()
     double fuel, fuel_efficiency, distance_left;
 
     //Getting the Vars Values
-    cout << "Please enter the cars fuel efficiency in (L/100km)";
+    cout << "Please enter the cars fuel efficiency in (L/100km) ";
     cin >> fuel_efficiency;
     cout << "Please enter the fuel remaining in the tank (L) ";
     cin >> fuel;
@@ -16,7 +16,7 @@ int main()
     distance_left = (fuel * 100) / fuel_efficiency;
 
     //returning the variable
-    cout << "You have" << distance_left << "kms left" << endl;
+    cout << "You have " << distance_left << "kms left" << endl;
     
     return 0;
 }
