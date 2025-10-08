a3cdaa0f20669411cfc07dd087e6bb3f8c1c3a30
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2025-10-08 16:37:32 -0700

    Update Pointer_Lab.cpp

--- Pointer_Lab.cpp
+++ Pointer_Lab.cpp
@@ -1,35 +1,65 @@
 #include <iostream>
-#include <cmath>
-#include <cstdlib>
 using namespace std;
 
-void get_number_of_rolls(int &rolls);
-int roll_dice(int &rolls);
-void display_result(int rolls, int numbers[]);
+struct Point {
+  double x;
+  double y;
+};
 
-int main() 
+void get_point(Point* p);                      // read x/y coordinates from the user into a Point p
+void get_move_coordinates(float& x, float& y); // get the x and y coordinates to move the point
+void move_point(Point& p, float x, float y);   // add the x and y offsets to the Point p
+void print_point(Point* p);                      // print our position of Point P neatly to the screen
+
+
+void get_point(Point* pt)
 {
-  srand(time(0));
-  int distribution_of_numbers[5] = {0};
-  int number_of_rolls;
-  int *number_pointer = &number_of_rolls;
-  get_number_of_rolls
-    return 0;
+  cout << "Please enter the coordinates for the point: " << endl;
+  cin >> pt->x >> pt->y;
 }
 
-void display_result(int &rolls, int numbers[])
+void get_move_coordinates(float& x, float& y)
 {
-  
+  cout << "Please enter the move coordinates (x y): " << endl;
+  cin >> x >> y;
 }
 
-int roll_dice(int &rolls)
+void move_point(Point& p, float x, float y)
 {
-  
+  p.x += x;
+  p.y += y;
 }
 
-void get_number_of_rolls(int &rolls)
+void print_point(Point* p)
 {
-  cout << "Let’s roll some dice!" << endl << "Please enter the number of rolls to make: ";
-  cin >> &rolls;
-  cout << "Rolling...";
+  cout << "The new position of the point is: (4, 8) " << p->x << " " << p->y;
 }
+
+int main ()
+{
+  float move_x, move_y;
+  Point *p = new Point;
+    // read in point 
+  get_point (p);
+    // read in move coordinates
+  get_move_coordinates(move_x, move_y);
+    // calculate new point position
+  move_point(*p, move_x, move_y);
+  print_point(p);
+   return 0;
+}
+
+
+/* 
+Definition:
+Write a program that uses a struct to represent a Point 
+in 2D space. The program should allow the user to input 
+the coordinates for a point, and a move position (x/y) 
+and the program will output the new position of the 
+point. Use pointers/references for passing the point 
+variable to functions for input, movement and distance.
+
+Please enter the coordinates for the point: 1 3
+Please enter the move coordinates (x y): 3 5
+The new position of the point is: (4, 8)
+*/
