9e6d70fd18a0fb66fca1d18e2b25c1ba4eb8b7c6
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2025-10-08 14:55:26 -0700

    Update Pointer_Lab.cpp

--- Pointer_Lab.cpp
+++ Pointer_Lab.cpp
@@ -1,1 +1,35 @@
 #include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+void get_number_of_rolls(int &rolls);
+int roll_dice(int &rolls);
+void display_result(int rolls, int numbers[]);
+
+int main() 
+{
+  srand(time(0));
+  int distribution_of_numbers[5] = {0};
+  int number_of_rolls;
+  int *number_pointer = &number_of_rolls;
+  get_number_of_rolls
+    return 0;
+}
+
+void display_result(int &rolls, int numbers[])
+{
+  
+}
+
+int roll_dice(int &rolls)
+{
+  
+}
+
+void get_number_of_rolls(int &rolls)
+{
+  cout << "Let’s roll some dice!" << endl << "Please enter the number of rolls to make: ";
+  cin >> &rolls;
+  cout << "Rolling...";
+}
