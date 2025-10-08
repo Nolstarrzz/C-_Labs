224dc44990eefe395f8651c81562470ba95d56ab
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2025-10-08 16:30:30 -0700

    Update assignment_4.cpp

--- Assignments/Assignment_4/assignment_4.cpp
+++ Assignments/Assignment_4/assignment_4.cpp
@@ -3,19 +3,19 @@
 #include <cstdlib>
 using namespace std;
 
-void get_number_of_rolls(int &rolls);
-void roll_dice(int &rolls, int *pointer);
-void display_result(int numbers[]);
+void get_number_of_rolls(long &rolls);
+void roll_dice(long &rolls, long *pointer);
+void display_result(long numbers[]);
 int snake_eyes_roller();
-void display_result_snake_eyes(int value);
+void display_result_snake_eyes(long value);
 
 int main() 
 {
   srand(time(0));
-  int distribution_of_numbers[6] = {0};
-  int total;
-  int *numbers_pointer = &distribution_of_numbers[0];
-  int number_of_rolls;
+  long distribution_of_numbers[6] = {0};
+  long total;
+  long *numbers_pointer = &distribution_of_numbers[0];
+  long number_of_rolls;
   get_number_of_rolls(number_of_rolls);
   roll_dice(number_of_rolls, numbers_pointer);
   display_result(distribution_of_numbers);
@@ -25,13 +25,13 @@ int main()
   return 0;
 }
 
-void display_result_snake_eyes(int value)
+void display_result_snake_eyes(long value)
 {
   cout << endl << "Trying to roll double 1s..." << endl 
   << "omg it took " << value << " rolls to get snake eyes!";
 }
 
-void display_result(int numbers[])
+void display_result(long numbers[])
 {
   cout << "1 was rolled " << numbers[0] <<  " times"
   << endl << "2 was rolled " << numbers[1] <<  " times"
@@ -71,16 +71,16 @@ int snake_eyes_roller()
     return total_rolls;
 }
 
-void roll_dice(int &rolls, int *pointer)
+void roll_dice(long &rolls, long *pointer)
 {
-  for(int i = 0; i < rolls; i++)
+  for(long i = 0; i < rolls; i++)
   {
     int temp_roll = rand() % 6;
     pointer[temp_roll]++;
     }
 }
 
-void get_number_of_rolls(int &rolls)
+void get_number_of_rolls(long &rolls)
 {
   cout << "Let’s roll some dice!" << endl << "Please enter the number of rolls to make: ";
   cin >> rolls;
