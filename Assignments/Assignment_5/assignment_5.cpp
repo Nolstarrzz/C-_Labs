64c5e79e3913d6ff68a2845c900d825c26b45fa8
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2025-11-03 08:38:05 -0800

    Update assignment_5.cpp

--- Assignments/Assignment_5/assignment_5.cpp
+++ Assignments/Assignment_5/assignment_5.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cctype>
+#include <cmath>
 using namespace std;
 //so get a line, then find if its a # or ., then print the symbol
 //and keep looking for it there are more and increment a counter,
@@ -11,22 +13,26 @@ using namespace std;
 string evaluateLineOfString(string line);
 string getFileName();
 void splitString(vector<string> &lines);
+void findString(vector<string> &line, ifstream &filename);
+string revertString(string line);
 
-const string ascii_pic = "##.....##.########.##.....##.\n"
-                         "##.....##.##.......##.....##.\n"
-                         "##.....##.##.......##.....##.\n"
-                         "##.....##.######...##.....##.\n"
-                         "##.....##.##........##...##..\n"
-                         "##.....##.##.........##.##...\n"
-                         ".#######..##..........###....\n";
+const string ascii_pic = "########.##....##..######...########.....##...########..#######..\n"
+                         "##.......###...##.##....##..##.....##..####...##.......##.....##.\n"
+                         "##.......####..##.##........##.....##....##...##..............##.\n"
+                         "######...##.##.##.##...####.########.....##...#######...#######..\n"
+                         "##.......##..####.##....##..##...##......##.........##........##.\n"
+                         "##.......##...###.##....##..##....##.....##...##....##.##.....##.\n"
+                         "########.##....##..######...##.....##..######..######...#######..\n";
+
 
 int main()
 {
     ofstream outfile;
-    //ifstream infile;
+    ifstream infile;
     string resultingString;
-    vector<string> lines; 
-
+    vector<string> lines;
+    vector<string> newline;
+    double encodedLength = 0.0;
     string filename = getFileName();
     outfile.open(filename);
     if(outfile.fail())
@@ -37,8 +43,50 @@ int main()
     splitString(lines);
     for(string s : lines)
     {
+        outfile << evaluateLineOfString(s) << endl;
         cout << evaluateLineOfString(s) << endl;
     }
+    outfile.close();
+    infile.open(filename);
+    if(infile.fail())
+    {
+        cout << "failed!!";
+        exit(1);
+    }
+    findString(newline, infile);
+    for (string s: newline)
+    {
+        cout << revertString(s) << endl;
+        encodedLength += s.length();
+    }
+    infile.close();
+    cout << "Source image size: " << ascii_pic.length() << endl 
+    << "Encoded image size: " << encodedLength << endl << "Encoding is " 
+    << abs(((encodedLength/ascii_pic.length()) * 100) - 100) << "% smaller!";
+}
+
+string revertString(string line)
+{
+    string returnString;
+    int i=0;
+    while(i<line.length())
+    {
+        char currentChar = line[i];
+        int numstart = i+1;
+        int numend = i+1;
+        while(isdigit(line[numend])) 
+        {
+            numend++;
+        }
+        string currentNumber = line.substr(numstart, numend - numstart);
+        int currentNum = stoi(currentNumber);
+        for(int j = 0; j<currentNum; j++)
+        {
+            returnString += currentChar;
+        }
+        i = numend;
+    }
+    return returnString;
 }
 
 void splitString(vector<string> &lines)
@@ -53,6 +101,13 @@ void splitString(vector<string> &lines)
         endPosition = ascii_pic.find('\n', startPosition);
     }
 
+}
+
+void findString(vector<string> &line, ifstream &filename)
+{
+    string currentline;
+    while(getline(filename, currentline))
+        line.push_back(currentline);
 }
 
 string evaluateLineOfString(string line)
@@ -83,3 +138,12 @@ string getFileName()
     cin >> file;
     return file;
 }
+
+/*
+Line 5: Added a 4 into the parameters as string deck[][13] does not show the amount of string arrays in the array
+Line 20: (drawn+i) = deck[suit][number]; This is wrong becasue it isnt accessing the value at the location so the *(drawn+i)
+needs an asterisk in front of it to access the contents
+Line 26: The line was checking the drawn then incrementing afterwards and moving the memory address itself reather then 
+checking if it were the same card. SO the fix was to make it add the i value
+Line 38-42: The array wasnt being initialized properly as it was using square brackets, the fix is to use {} to initialize the array
+*/
